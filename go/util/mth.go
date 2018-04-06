bb16f07bf0de8b8fd47357f9e88e9140203dd288
Kurt <kurt.w.jung@gmail.com>
2018-04-06 18:31:57 -0400

    Averaging structure and methods

--- go/util/mth.go
+++ go/util/mth.go
@@ -37,3 +37,58 @@ func (eq LinearEquationType) Perpendicular(x float64) (p LinearEquationType) {
 	}
 	return
 }
+
+// LinearY returns the value of the linear function defined by intercept and
+// slope at the specifed x value.
+func LinearY(slope, intercept, x float64) (y float64) {
+	y = slope*x + intercept
+	return
+}
+
+// Linear returns the y-intercept and slope of the straight line joining the
+// two specified points. For scaling purposes, associate the arguments as
+// follows: x1: observed low value, y1: desired low value, x2: observed high
+// value, y2: desired high value.
+func Linear(x1, y1, x2, y2 float64) (eq LinearEquationType) {
+	if x2 != x1 {
+		eq.Slope = (y2 - y1) / (x2 - x1)
+		eq.Intercept = y2 - x2*eq.Slope
+	}
+	return
+}
+
+// LinearPointSlope returns the y-intercept of the straight line joining the
+// specified arbitrary point (not necessarily an intercept) and the line's
+// slope.
+func LinearPointSlope(x, y, slope float64) (intercept float64) {
+	intercept = y - slope*x
+	return
+}
+
+// AverageType manages the calculation of a running average
+type AverageType struct {
+	weight float64
+	value  float64
+}
+
+// Add adds a value to a running average. weight is quietly constrained to the
+// range [0, 1].
+func (avg *AverageType) Add(val, weight float64) {
+	if weight > 0 {
+		if weight < 0 {
+			weight = 0
+		} else if weight > 1 {
+			weight = 1
+		}
+		oldWeight := avg.weight
+		avg.weight += weight
+		// 	val = weight*val + (1-weight)*avg.value
+		// 	avg.weight += 1
+		avg.value = (avg.value*oldWeight + val*weight) / avg.weight
+	}
+}
+
+// Value returns the current average of submitted values
+func (avg AverageType) Value() float64 {
+	return avg.value
+}
